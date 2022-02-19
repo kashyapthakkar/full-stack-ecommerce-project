0895ec55b046023928cf1894570f660ba02a1417
kashyapthakkar <47193812+kashyapthakkar@users.noreply.github.com>
2022-02-18 23:29:22 -0500

    Checkout page added/ navigation bar added

--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -1,49 +1,80 @@
 <template>
-    <h1>Cart Page</h1>    
+  <div id="page-wrap">
+    <h1>Shopping Cart</h1>
+    <div
+      v-for="product in cartItems"
+      :key="product.id"
+      class="product-container"
+    >
+      <img class="product-image" :src="product.imageUrl" />
+      <div class="details-wrap">
+        <h3>{{ product.name }}</h3>
+        <p>${{ product.price }}</p>
+      </div>
+      <button class="remove-button">Remove From Cart</button>
+    </div>
+    <h3 id="total-price">Total: ${{ totalPrice }}</h3>
+    <button id="checkout-button">Proceed to Checkout</button>
+  </div>
 </template>
 <script>
-export default{
-    name: 'CartPage'
-}
+import { cartItems } from "../fake-data";
+export default {
+  name: "CartPage",
+  data: function () {
+    return {
+      cartItems,
+    };
+  },
+  computed: {
+    totalPrice: function () {
+      let totalPrice = 0;
+      this.cartItems.filter(function (product) {
+        totalPrice += Number(product.price);
+      });
+      return totalPrice;
+    },
+  },
+};
 </script>
 <style scoped>
-  h1 {
-    border-bottom: 1px solid black;
-    margin: 0;
-    margin-top: 16px;
-    padding: 16px;
-  }
+h1 {
+  border-bottom: 1px solid black;
+  margin: 0;
+  margin-top: 16px;
+  padding: 16px;
+}
 
-  #total-price {
-    padding: 16px;
-    text-align: right;
-  }
+#total-price {
+  padding: 16px;
+  text-align: right;
+}
 
-  #checkout-button {
-    width: 100%;
-  }
+#checkout-button {
+  width: 100%;
+}
 
-  .product-container {
-    align-content: 'center';
-    border-bottom: 1px solid #ddd;
-    display: flex;
-    padding: 16px;
-    width: 100%;
-  }
+.product-container {
+  align-content: "center";
+  border-bottom: 1px solid #ddd;
+  display: flex;
+  padding: 16px;
+  width: 100%;
+}
 
-  .product-image {
-    flex: 1;
-    height: 100px;
-    max-width: 100px;
-  }
+.product-image {
+  flex: 1;
+  height: 100px;
+  max-width: 100px;
+}
 
-  .details-wrap {
-    padding: 0 16px;
-    flex: 3;
-  }
+.details-wrap {
+  padding: 0 16px;
+  flex: 3;
+}
 
-  .remove-button {
-    flex: 1;
-    margin: auto;
-  }
+.remove-button {
+  flex: 1;
+  margin: auto;
+}
 </style>
